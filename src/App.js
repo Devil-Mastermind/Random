119b0bc1134ec8072b99577a50275f02478eac44
DevilMastermind <arnavgupta6163@gmail.com>
2024-07-18 18:31:52 +0530

    okayy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import love from './images/love.png'
+import emo from './images/emo.png'
+import React from 'react';
+// import heart from 'https://static.vecteezy.com/system/resources/previews/001/187/438/original/heart-png.png'
+
+//1536  695
 
 function App() {
+  const heart = 'https://static.vecteezy.com/system/resources/previews/001/187/438/original/heart-png.png'
+
+  const [img, setImg] = React.useState(heart)
+  const [randomHeight, setRandomHeight] = React.useState(200)
+  const [randomWidth, setRandomWidth] = React.useState(650)
+  function handleNo() {
+    // console.log("NO");
+    setRandomHeight(Math.floor(Math.random()*400))
+    setRandomWidth(Math.floor(Math.random()*1000))
+    setImg(emo)
+    console.log(randomHeight);
+    console.log(randomWidth);
+
+  }
+  function handleYes() {
+    console.log("YES");
+    setImg(love)
+  }
+  const subStyle = {
+    // bottom : `${randomHeight}vh`,
+    // right : `${randomWidth}vw`
+    // bottom : `${randomHeight}vh` ,
+    // right : `${randomWidth}vw` ,
+    marginTop : `${randomHeight}px`,
+    marginLeft : `${randomWidth}px`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div className="sub-container" style={subStyle}>
+          <div className="heading">
+            Will You Marry Me?
+          </div>
+          <div className="photo">
+            <img src={img} className='img' />
+          </div>
+          <div className="btn-cont">
+            <div className="btn no" onMouseEnter={handleNo}>
+              Noi
+            </div>
+            <div className="btn yes" onMouseEnter={handleYes}>
+              Yeah
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
